889cec543f694dce43bd97c1fde985dcf0bfea6e
mesuara <mesuara@gmail.com>
2024-05-13 22:21:24 -0400

    added camera and image Layer class

---
+++ src/ImageLayer.java
@@ -0,0 +1,33 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+public class ImageLayer
+{
+	public Image image;
+
+	int x;
+	int y;
+	
+	int z;
+	
+	
+	public ImageLayer(String filename, int x, int y, int z)
+	{
+		image = Toolkit.getDefaultToolkit().getImage("../Image"+filename);
+	
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	
+	public void draw(Graphics pen)
+	{
+		for(int i = 0; i < 10; i++)
+		
+			pen.drawImage(image, x + i * 1600 - Camera.x / z, y - Camera.y / z, 1600, 800, null);
+	}
+	
+
+}
